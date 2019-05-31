adb4e62ed857604cbf1c9a4ac7cc926ca0cde965
Arash <arashfarahani@hotmail.com>
2019-05-31 17:28:51 +0430

    It is not important

--- src/main/java/com/hackerrank/github/repository/EventRepository.java
+++ src/main/java/com/hackerrank/github/repository/EventRepository.java
@@ -15,4 +15,5 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query(value = "SELECT * FROM EVENT A WHERE FK_ACTOR = :actorId ORDER BY EVENT_ID",
             nativeQuery = true)
     List<Event> findByActorIdOrderByIdAsc(@Param("actorId") Long actorId);
+    List<Event> findByActor_IdOrderByIdAsc(Long actorId);
 }
